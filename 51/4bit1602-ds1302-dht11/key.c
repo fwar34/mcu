a428fb1e070867549ed034b84e61212bc25dfd6a
fwar34 <fwar34@126.com>
2020-09-03 17:12:14 +0800

    update key

---
+++ 51/4bit1602-ds1302-dht11/key.c
@@ -0,0 +1,49 @@
+#include <reg52.h>
+
+sbit row1 = P3 ^ 4;
+sbit row2 = P3 ^ 5;
+sbit column1 = P3 ^ 6;
+sbit column2 = P3 ^ 7;
+
+unsigned char row;
+unsigned char column;
+
+void delay(unsigned short count)
+{
+    while (count--);
+}
+
+void key_scan()
+{
+    char a;
+    row1 = 0;
+    row2 = 0;
+    column1 = 1;
+    column2 = 1;
+    if (column1 == 0 || column2 == 0) { //检测2列中的哪一列被按下
+        delay(1000); //延时去抖
+        if (column1 == 0) {
+            column = 1;
+        } else if (column2 == 0) {
+            column = 2;
+        }
+    }
+
+    row1 = 1;
+    row2 = 1;
+    column1 = 0;
+    column2 = 0;
+    if (row1 == 0 || row2 == 0) {   //检测2行中的哪一行被按下
+        delay(1000); //延时去抖
+        if (row1 == 0) {
+            row = 1;
+        } else if (row2 == 0) {
+            row = 2;
+        }   
+    }
+
+    while (a < 50 && row1 && row2) { //延时，确保没有按键再按下
+        a++;
+        delay(1000);
+    }
+}
