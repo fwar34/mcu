41dce7f60443f5d623b7be7d44db3d0c34b22896
fwar34 <fwar34@126.com>
2020-09-17 07:57:52 +0800

    update

---
+++ 51/clock-11/clock-11/src/main.c
@@ -0,0 +1,120 @@
+//#include <stc8.h>
+#include "ds1302.h"
+#include "lcd1602.h"
+#include "dht11.h"
+#include "delay.h"
+#include "infrared.h"
+#include "common.h"
+
+sfr P0M0        =   0x94;
+sfr P0M1        =   0x93;
+sfr P1M0        =   0x92;
+sfr P1M1        =   0x91;
+sfr P2M0        =   0x96;
+sfr P2M1        =   0x95;
+sfr P3M0        =   0xb2;
+sfr P3M1        =   0xb1;
+sfr P4M0        =   0xb4;
+sfr P4M1        =   0xb3;
+sfr P5M0        =   0xca;
+sfr P5M1        =   0xc9;
+sfr P6M0        =   0xcc;
+sfr P6M1        =   0xcb;
+sfr P7M0        =   0xe2;
+sfr P7M1        =   0xe1;
+
+#define CKSEL           (*(unsigned char volatile xdata *)0xfe00)
+#define CKDIV           (*(unsigned char volatile xdata *)0xfe01)
+#define IRC24MCR        (*(unsigned char volatile xdata *)0xfe02)
+#define XOSCCR          (*(unsigned char volatile xdata *)0xfe03)
+#define IRC32KCR        (*(unsigned char volatile xdata *)0xfe04)
+sfr P_SW2 = 0xba;
+
+extern void Timer0Init(void);		//50毫秒@11.0592MHz
+extern unsigned short idle_count; //最后一次设置开始空闲计数
+extern unsigned int new_value;
+
+extern unsigned char ch_count; //两次ch键进入设置的时间计数
+
+sbit led = P0 ^ 5;
+void display_current_setting()
+{
+    write_char(0, 13, current_setting + '0');
+}
+
+void display_idle_count()
+{
+    /* unsigned char i = idle_count / 10; */
+    /* unsigned char j = idle_count % 10; */
+    unsigned char i = new_value / 10;
+    unsigned char j = new_value % 10;
+    write_char(0, 14, i + '0');
+    write_char(0, 15, j + '0');
+}
+
+void main(void)
+{
+    //初始时间20年8月16号14点16分55秒星期天 
+    DS1302_TIME start_time = {20, 9, 9, 3, 0, 6, 40};
+    DS1302_TIME current_time;
+
+    P_SW2 = 0x80;
+    XOSCCR = 0xc0;                              //启动外部晶振
+    while (!(XOSCCR & 1));                      //等待时钟稳定
+    CKDIV = 0x00;                               //时钟不分频
+    CKSEL = 0x01;                               //选择外部晶振
+    P_SW2 = 0x00;
+
+     /*P0M1 = 0x00;
+        P0M0 = 0x00;
+
+        P1M1 = 0x00;         
+        P1M0 = 0x00;
+
+        P2M1 = 0x00;
+        P2M0 = 0x00;
+
+        P3M1 = 0x00;
+        P3M0 = 0x00;
+
+        P4M1 = 0x00;
+        P4M0 = 0x00;
+
+        P5M1 = 0x00;
+        P5M0 = 0x00;
+
+        P6M1 = 0x00;
+        P6M0 = 0x00;
+
+        P7M1 = 0x00;
+        P7M0 = 0x00;*/
+    
+    initLcd1602();
+    ds1302_init();
+
+    if (!ds1302_is_running()) {
+        ds1302_write_time(&start_time);
+    }
+
+    Timer0Init();
+    beep_mute();
+    IrInit();
+    lcd_light_back = 1;
+    
+    while (1)
+    {
+        display_current_setting();
+        display_idle_count();
+        if (current_setting) {
+            enter_settings();
+        } else {
+            ds1302_read_time(&current_time);
+            display(&current_time);
+        }
+        display_dht11();
+        
+        //lcd_light_back = !lcd_light_back;
+        //led = !led;
+        //Delay1000ms();
+    }
+}
---
+++ 51/clock-11/clock-11/src/timer.c
@@ -0,0 +1,83 @@
+#include "dht11.h"
+#include "key.h"
+#include "ds1302.h"
+#include "common.h"
+
+extern unsigned char ch_count; //两次ch键进入设置的时间计数
+extern bit first_ch_flag; //表示第一次按ch的标志
+extern unsigned char dht11_data[5]; //湿度十位，湿度个位，温度十位，温度个位，是否显示的标志
+extern unsigned short idle_count; //最后一次设置开始空闲计数
+
+static unsigned short count = 0; //dht11更新的计数
+
+/* void Timer0Init(void)		//50毫秒@11.0592MHz */
+/* { */
+/*     TMOD = 0x01;                    //set timer0 as mode1 (16-bit) */
+/*     TH0 = 0x4C; */
+/*     TL0 = 0x00; */
+
+/*     TF0 = 0;		//清除TF0标志 */
+/*     TR0 = 1;		//定时器0开始计时 */
+
+/*     ET0  = 1;                           //enable timer0 interrupt */
+/*     EA  = 1;                           //open global interrupt switch */
+/* } */
+
+void Timer0Init(void)		//10毫秒@24.000MHz
+{
+    /* AUXR &= 0x7F;		//定时器时钟12T模式 */
+    TMOD &= 0xF0;		//设置定时器模式
+    TMOD |= 0x01;		//设置定时器模式
+    TL0 = 0xE0;		//设置定时初值
+    TH0 = 0xB1;		//设置定时初值
+    TF0 = 0;		//清除TF0标志
+    TR0 = 1;		//定时器0开始计时
+
+    ET0  = 1;                           //enable timer0 interrupt
+    EA  = 1;                           //open global interrupt switch
+}
+
+
+
+/* https://zhidao.baidu.com/question/477148677.html */
+/* void Timer0Delay1s() interrupt 1 */
+/* { */
+/*     static unsigned char count; */
+/*     TH0 = (65535 - 50000) / 256; */
+/*     TL0 = (65535 - 50000) % 256; */
+/*     if (count ++ == 20) { */
+/*         count = 0; */
+/*         //TODO */
+/*     } */
+/* } */
+
+/* Timer0 interrupt routine */
+void tm0_isr() interrupt 1
+{
+    TL0 = 0xE0;		//设置定时初值
+    TH0 = 0xB1;		//设置定时初值
+    if (ch_count > 0) { //第一次点击ch按钮会把ch_count设置成1
+        ++ch_count;
+        if (ch_count > 100) { //1s过后没有点击第二次ch按钮的话重置字段
+            ch_count = 0;
+        }
+    }
+
+    if (idle_count > 0) { //每一次设置会把idle_count设置成1,所以大于0才判断是否是设置空闲超时
+        ++idle_count;
+        if (idle_count > 100 * 10) { //设置空闲了10秒之后退出
+            exit_settings();
+        }
+    }
+
+    if (++count == 100 * 2) {                //1000ms * 2 -> 2s更新一次dht11
+        /* lcd_light_back = !lcd_light_back ; */
+        count = 0;                //reset counter
+        if (!dht11_read_data()) { 
+            //read success
+            dht11_data[5] = 1;
+        }
+    }
+
+    process_key(); //处理物理按键
+}
