204d8630fd7769a51d3f898402252edb52fad4f7
fwar34 <fwar34@126.com>
2020-09-29 21:03:37 +0800

    update

--- stm8/GPIO_Toggle/src/stm8s_conf.h
+++ stm8/GPIO_Toggle/src/stm8s_conf.h
@@ -1,29 +1,27 @@
-/**
-******************************************************************************
-* @file     stm8s_conf.h
-* @author   MCD Application Team
-* @version  V2.0.4
-* @date     26-April-2018
-* @brief    This file is used to configure the Library.
-******************************************************************************
-* @attention
-*
-* <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
-*
-* Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-* You may not use this file except in compliance with the License.
-* You may obtain a copy of the License at:
-*
-*        http://www.st.com/software_license_agreement_liberty_v2
-*
-* Unless required by applicable law or agreed to in writing, software 
-* distributed under the License is distributed on an "AS IS" BASIS, 
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-******************************************************************************
-*/ 
+/********************************************************************************
+ * @file     stm8s_conf.h
+ * @author   MCD Application Team
+ * @version  V2.0.4
+ * @date     26-April-2018
+ * @brief    This file is used to configure the Library.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+ *
+ * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *        http://www.st.com/software_license_agreement_liberty_v2
+ *
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *******************************************************************************/ 
 
 /* SDCC patch: include "STM8AF622x" defined in "STM8S_StdPeriph_Tempate" */
 
@@ -107,7 +105,7 @@
  */
 #define assert_param(expr) ((expr) ? (void)0 : assert_failed((uint8_t *)__FILE__, __LINE__))
 /* Exported functions ------------------------------------------------------- */
-    void assert_failed(uint8_t* file, uint32_t line);
+        void assert_failed(uint8_t* file, uint32_t line);
 #else
 #define assert_param(expr) ((void)0)
 #endif /* USE_FULL_ASSERT */
