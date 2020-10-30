19efcd86d4d80cfbcdb6ce6ca65cdc361d87d764
fwar34 <fwar34@126.com>
2020-10-30 18:55:53 +0800

    modify aos_list.*

---
+++ task/aos_stm8_iar/aos_list.c
@@ -0,0 +1,323 @@
+#include <intrinsics.h>
+#include <stdint.h>
+#include <string.h>
+#include <iostm8.h>
+#include "aos_list.h"
+#include "aos_config.h"
+#include "circle_queue.h"
+#include "uart_sdcc.h"
+
+extern void archFirstThreadRestore(uint8_t* new_stack_ptr);
+extern void archContextSwitch(uint8_t** old_stack_ptr, uint8_t* new_stack_ptr);
+
+//static uint8_t* stack_temp = NULL;
+
+#define IDLE_TASK_TID MAX_TASKS
+#define TASK_SAVE()                             \
+    asm("ldw Y, 0x08"); /*?b8,?b9*/             \
+    asm("pushw Y");                             \
+    asm("ldw Y, 0x0A"); /*?b10,?b11*/           \
+    asm("pushw Y");                             \
+    asm("ldw Y, 0x0C"); /*?b12,?b13*/           \
+    asm("pushw Y");                             \
+    asm("ldw Y, 0x0E"); /*?b14,?b15*/           \
+    asm("pushw Y");                             \
+    asm("ldw Y, SP");                           \
+    asm("ldw l:stack_temp, Y");
+
+#define TASK_RESTORE()                          \
+    asm("ldw Y, l:stack_temp");                 \
+    asm("ldw SP, Y");                           \
+    asm("popw Y");                              \
+    asm("ldw 0x0E, Y"); /*?b14,?b15*/           \
+    asm("popw Y");                              \
+    asm("ldw 0x0C, Y"); /*?b12,?b13*/           \
+    asm("popw Y");                              \
+    asm("ldw 0x0A, Y"); /*?b10,?b11*/           \
+    asm("popw Y");                              \
+    asm("ldw 0x08, Y"); /*?b8,?b9*/
+
+
+/* typedef enum { */
+/*     TASK_INVALID, */
+/*     TASK_SUSPEND, */
+/*     TASK_BLOCK, */
+/*     TASK_DELAY, */
+/*     TASK_READY, */
+/*     TASK_RUNNING */
+/* } TaskStatus; */
+
+typedef struct {
+    uint8_t semaphore;
+    List tcb_list;
+} Semaphore;
+
+typedef struct {
+    uint8_t event;
+    List tcb_list;
+} Event;
+
+typedef struct {
+    uint8_t* stack_ptr;
+    uint8_t tid;
+    task_func task;
+    uint16_t delay_ticks;
+    uint8_t stack[MAX_TASK_STACK_LENGTH];
+} TCB_Info;
+
+typedef struct {
+    uint8_t is_init;
+    TCB_Info* current_tcb;
+    List ready_list;
+    List event_list;
+    List semaphore_list;
+    List delay_list;
+    uint16_t tid;
+} AOS_Info;
+
+AOS_Info aos = {
+    .current_tid = 0,
+    .is_init = 0,
+};
+
+void task_shell()
+{
+    __enable_interrupt();
+    if (aos.current_tcb->task) {
+        (aos.current_tcb->task)();
+    }
+}
+
+void aos_task_idle()
+{
+    while (1);
+}
+
+void aos_init()
+{
+    if (is_init) {
+        return;
+    }
+
+    list_init(&aos.ready_list, LIST_ASCENDING);
+    list_init(&aos.delay_list, LIST_ASCENDING);
+    list_init(&aos.event_list, LIST_ASCENDING);
+    list_init(&aos.semaphore_list, LIST_ASCENDING);
+
+    //添加空闲任务,优先级最小255
+    static TCB_Info idle_tcb;
+    static Node idle_node;
+    idle_node.priority = 255;
+    idle_node.data = &idle_tcb;
+    list_insert_ascending(&aos.ready_list, &idle_node);
+    idle_tcb.task = aos_task_idle;
+    idle_tcb.stack_ptr = idle_tcb.stack + MAX_TASK_STACK_LENGTH - 1;
+    *(idle_tcb.stack_ptr)-- = (uint16_t)task_shell & 0xFF;
+    *(idle_tcb.stack_ptr)-- = (uint16_t)task_shell >> 8;
+    idle_tcb.stack_ptr -= 8; //?b8->?b15
+    idle_tcb.tid = ++tid;
+
+    is_init = 1;
+}
+
+uint8_t event_create(Event* event)
+{
+}
+
+void event_delete(uint8_t event)
+{
+    if (!event || event > MAX_EVENT_VECTOR) {
+        return;
+    }
+
+    aos.event_vector[event] = 0xFF;
+}
+
+uint8_t event_push(uint8_t event)
+{
+    if (event == 0 || event > MAX_EVENT_VECTOR) {
+        return 0;
+    }
+
+    if (aos.event_vector[event] == 0xFF) {
+        return 0;
+    }
+
+    //这里必须用临界区保护，因为不保护的话在push和设置TASK_READY中间被中断ISR调度的话，
+    //当前task是SUSPEND会改成READY后可能立即被执行一次，等当前任务下次重新被切入的时候又会在这里把状态设置成READY，
+    //就可能再次被执行了一次，第二次pop就会出错，因为已经被第一次pop出去了
+    __disable_interrupt();
+    push(&aos.tcb_info[aos.event_vector[event]].event_queue, &event);
+    /* if (event == 1) { */
+    /*     uart_send_string("push 1\n"); */
+    /* } */
+    aos.tcb_info[aos.event_vector[event]].status = TASK_READY;
+    __enable_interrupt();
+
+    return 1;
+}
+
+uint8_t event_pop(uint8_t* event)
+{
+    if (empty(&aos.tcb_info[aos.current_tid].event_queue)) {
+        uart_send_string("event_queue empty!\n");
+        return 0;
+    }
+
+    if (!pop(&aos.tcb_info[aos.current_tid].event_queue, event)) {
+        return 0;
+    }
+    
+    /* if (*event == 1) { */
+    /*     uart_send_string("pop 1\n"); */
+    /* } */
+    return 1;
+}
+
+void event_wait(uint16_t ticks)
+{
+    if (!empty(&aos.tcb_info[aos.current_tid].event_queue)) {
+        return;
+    }
+
+    if (ticks == 0) {
+        //永久等待
+        aos.tcb_info[aos.current_tid].status = TASK_SUSPEND;
+    } else {
+        //超时等待
+        aos.tcb_info[aos.current_tid].delay_ticks = ticks;
+        aos.tcb_info[aos.current_tid].status = TASK_BLOCK;
+    }
+
+    aos_task_switch();
+}
+
+void aos_start()
+{
+    if (!aos.is_init) {
+        return;
+    }
+
+    aos.current_tcb = list_get_min(&aos.ready_list)->data;
+    archFirstThreadRestore(aos.current_tcb->stack_ptr);
+    __enable_interrupt();   
+}
+
+void aos_task_exit()
+{
+    aos.tcb_info[aos.current_tid].status = TASK_INVALID;
+    aos_task_switch();
+}
+
+void aos_task_switch()
+{
+    __istate_t _istate = __get_interrupt_state();    
+    __disable_interrupt();
+
+    uint8_t old_tid = aos.current_tid;
+    if (aos.tcb_info[old_tid].status == TASK_RUNNING) {
+        aos.tcb_info[old_tid].status = TASK_READY;
+    }
+    uint8_t new_tid = aos_get_next_task();
+    aos.tcb_info[new_tid].status = TASK_RUNNING;
+
+    if (old_tid != new_tid) {
+        if (aos.tcb_info[old_tid].status != TASK_INVALID) {
+            archContextSwitch(&aos.tcb_info[old_tid].stack_ptr, aos.tcb_info[new_tid].stack_ptr);
+        } else {
+            //如果当前任务已经删除则不保存当前任务的上下文
+            archFirstThreadRestore(aos.tcb_info[new_tid].stack_ptr);
+        }
+    }
+
+    __set_interrupt_state(_istate);
+}
+
+int8_t aos_task_load(task_func task)
+{
+    uint8_t i;
+    TCB_Info* info = NULL;
+    for (i = 0; i < MAX_TASKS; ++i) {
+        info = &aos.tcb_info[i];
+        if (info->status == TASK_INVALID) {
+            memset(info, 0, sizeof(TCB_Info));
+            info->task = task;
+            info->stack_ptr = info->stack + MAX_TASK_STACK_LENGTH - 1;
+            *(info->stack_ptr)-- = (uint16_t)task_shell & 0xFF;
+            *(info->stack_ptr)-- = (uint16_t)task_shell >> 8;
+            info->stack_ptr -= 8; //?b8->?b15
+            info->tid = i;
+            info->status = TASK_READY;
+            return i;
+        }
+    }
+    return -1;
+}
+
+void aos_task_weakup(uint8_t tid)
+{
+    if (tid >= MAX_TASKS) {
+        return;
+    }
+    aos.tcb_info[tid].status = TASK_READY;
+    aos_task_switch();
+}
+
+/* void aos_task_suspend() */
+/* { */
+/*     aos.tcb_info[aos.current_tid].status = TASK_BLOCK; */
+/*     aos_task_switch(); */
+/* } */
+
+void aos_task_sleep(uint16_t ticks)
+{
+    if (!ticks) {
+        return;
+    }
+    aos.tcb_info[aos.current_tid].delay_ticks = ticks;
+    aos.tcb_info[aos.current_tid].status = TASK_DELAY;
+    aos_task_switch();
+}
+
+//========时钟中断函数========================================================
+#pragma vector = TIM3_OVR_UIF_vector
+__interrupt void tim_isr()
+{
+    TIM3_SR1_UIF = 0;
+    //https://mp.weixin.qq.com/s/ScX5Y50K9jD6VUnORkWsmw?
+    //stm8这里不用关中断，因为tim3的中断优先级默认就是3，最高优先级，不会被其他可屏蔽的中断打断
+    //__istate_t _istate = __get_interrupt_state();    
+    //__disable_interrupt();
+    
+    uint8_t old_tid = aos.current_tid;
+    if (aos.tcb_info[old_tid].status == TASK_RUNNING) {
+        aos.tcb_info[old_tid].status = TASK_READY;
+    }
+
+    uint8_t i;
+    for (i = 0; i < MAX_TASKS; ++i) {
+        TCB_Info* info = &aos.tcb_info[i];
+        if (info->status == TASK_BLOCK || info->status == TASK_SUSPEND) {
+            if (!empty(&info->event_queue)) {
+                info->status = TASK_READY;
+                info->delay_ticks = 0;
+            }
+        }
+
+        if (info->status == TASK_BLOCK || info->status == TASK_DELAY) {
+            if (info->delay_ticks > 0) {
+                if (--info->delay_ticks == 0) {
+                    info->status = TASK_READY;
+                }
+            }
+        }
+    }
+
+    uint8_t new_tid = aos_get_next_task();
+    aos.tcb_info[new_tid].status = TASK_RUNNING;
+    if (old_tid != new_tid) {
+        archContextSwitch(&aos.tcb_info[old_tid].stack_ptr, aos.tcb_info[new_tid].stack_ptr);
+    }
+
+    //__set_interrupt_state(_istate);
+}
---
+++ task/aos_stm8_iar/aos_list.h
@@ -0,0 +1,85 @@
+#ifndef _AOS_H_
+#define _AOS_H_
+
+#include <stdint.h>
+/*
+  关于消息机制:
+  该系统的消息机制其实并不是真正的消息机制,其过程是依靠休眠和唤醒来实现的.机制是这样的:
+
+  当一个进程需要等待某消息时,实际上是将进程的task_id保存在消息向量表中,然后进入休眠/延时状态.
+  此后事件发生时,系统直接根据消息号从向量表中找到等待它的进程的task_id,然后将其唤醒.整个过程中并没有真正的消息在传递.
+
+  使用这机的方式实现消息,执行效率较高,但有一些限制,使用时应注意并避过:
+  1.进程必须在进入休眠/延时后才能收到消息
+  2.进程虽可同时等待多个消息,但由于无法知道消息的内容,所以除非在事件发生处额外作标记(定义变量)供进程辨认,否则不要同时等待多个消息
+*/
+
+typedef void (*task_func)();
+//注册指定消息.当该消息发生时,系统将唤醒注册者.
+/* #define event_replace(eid) event_vector[eid] = task_id */
+//清除指定消息
+/* #define event_clear(eid) event_vector[eid] = 0xFF */
+//保存指定消息到reg中(unsigned char类型)的原注册者,并监听该消息.
+/* #define event_reg(eid, reg) reg = event_vector[eid], event_vector[eid] = task_id */
+//保存指定消息到reg中(unsigned char类型)的原注册者,并监听该消息.
+uint8_t event_reg(uint8_t event);
+//从reg中(unsigned char类型)恢复指定消息的原注册者
+/* #define event_unreg(eid, reg) event_vector[eid] = reg */
+void event_unreg(uint8_t event);
+//发送消息(严格地说,是唤醒监听者)
+/* #define event_push(eid) { if(event_vector[eid] != 0xFF) task_wakeup(event_vector[eid]); } */
+//发送消息(严格地说,是唤醒监听者)
+uint8_t event_push(uint8_t event);
+uint8_t event_pop(uint8_t* event);
+//初始化
+void aos_init();
+//启动任务调度.从最先添加的任务开始执行.
+void aos_start();
+//退出当前任务.如该任务注册了信号,则必须在退出前恢复/清理
+void aos_task_exit();
+//立即释放CPU,并在执行过其它全部未休眠的进程后回到释放点
+void aos_task_switch();
+//装载进程.如果任务槽满,则延时两个定时器中断周期,直到有空槽为止
+int8_t aos_task_load(task_func task);
+//设置指定的进程为休眠状态.
+/* #define task_setsuspend(tid) task_sleep[tid] = 0xFF; */
+//设置指定的进程为延时状态
+/* #define task_setsleep(tid, timer) task_sleep[tid] = timer */
+void aos_task_sleep(uint16_t ticks);
+//唤醒指定的进程.
+/* #define task_wakeup(tid) task_sleep[tid] = 0 */
+void aos_task_weakup(uint8_t tid);
+//进程立即进入延时
+/* #define task_sleep(timer) task_sleep[task_id] = timer, task_switch() */
+//进程立即进入休眠
+/* #define task_suspend() task_sleep[task_id] = 0xFF, task_switch() */
+//进程立即进入休眠
+/* void aos_task_suspend(); */
+void event_wait(uint16_t ticks);
+//执行指定语句后进入休眠.与直接调用task_suspend()相比,可防止中断中的消息在提前到达于进入休眠等待前.
+/* #define task_wait_interrupt(prog) { task_setsuspend(task_id); prog; task_switch(); } */
+/*task_wait_interrupt()的详细说明:
+  出于执行效率的考虑,该系统的消息发送是有"BUG"的:进程必须未在活动中("未活动"是指 1.进入休眠 2.延时状态后)才能收到消息.
+  具休地说就是,如果进程监听的消息产生于中断中,而该监听者当前正处于活动状态,则进程无法收到消息,
+
+  假定以下应用环境:
+
+  1.进程T1监听消息S1,然后向缓冲区BUFF写数据,接下来等待S1的产生
+  2.中断服务程序检查缓冲区BUFF,发现有数据则处理,并在处理完后发送信号S1
+
+  现在假定T1刚写完缓冲区,中断正好发生,并在处理完后发送信号S1,而此时T1并未进入信号等待阶段.
+  当中断服务退出后,T1不知道S1已经发生了,继续按预定程序进入信号等待阶段,于是死在那里.
+
+  调用task_wait_interrupt()宏可解决该问题.该宏实际上是先置进程为等待状态,然后才执行缓冲区操作.
+  用户可按自已的喜好选择调用该宏,或直接书写为展开之后的代码:
+
+  task_setsuspend(task_id);
+  操作缓冲区的代码;
+  task_switch();
+*/
+//与task_wait_interrupt()类似,但只在指定的时间内等待,而task_wait_interrupt()会永远等待.
+/* #define task_sleep_interrupt(prog) task_setsleep(task_id), prog, task_switch() */
+//结束指定的进程
+/* #define task_delete(tid) task_sp[tid] = 0 */
+
+#endif
