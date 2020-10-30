e48566a0d6be5b36baa70d8d73a2a1de90eb9d59
fwar34 <fwar34@126.com>
2020-10-30 14:22:18 +0800

    add list

---
+++ task/aos_stm8_iar/common_list.c
@@ -0,0 +1,175 @@
+#include "common_list.h"
+
+void list_init(List* list, uint8_t list_type)
+{
+    list->list_type = list_type;
+    list->list_length = 0;
+    list->begin = NULL;
+    list->end = NULL;
+    list->index = NULL;
+}
+
+void list_insert_first(List* list, Node* node)
+{
+    list->begin = node;
+    list->end = node;
+    node->prev = node;
+    node->next = node;
+    node->list = list;
+    ++list->list_length;
+}
+
+void list_insert_end(List* list, Node* node)
+{
+    if (list->list_length == 0) {
+        list_insert_first(list, node);
+    } else {
+        node->prev = list->end;
+        node->next = list->begin;
+        list->begin->prev = node;
+        list->end->next = node;
+        list->end = node;
+        node->list = list;
+        ++list->list_length;
+    }
+}
+
+void list_insert_begin(List* list, Node* node)
+{
+    if (list->list_length == 0) {
+        list_insert_first(list, node);
+    } else {
+        node->next = list->begin;
+        node->prev = list->end;
+        list->begin->prev = node;
+        list->end->next = node;
+        node->list = list;
+        list->begin = node;
+        ++list->list_length;
+    }
+}
+
+void list_remove_last()
+{
+    list->list_length = 0;
+    list->begin = NULL;
+    list->end = NULL;
+    list->index = NULL;
+}
+
+void list_remove_begin(List* list)
+{
+    list->begin = list->begin->next;
+    list->begin->prev = list->end;
+    list->end->next = list->begin;
+    --list->list_length;
+}
+
+void list_remove_end(List* list)
+{
+    list->end = list->end->prev;
+    list->end->next = list->begin;
+    list->begin->prev = list->end;
+    --list->list_length;
+}
+
+Node* list_get_max(List* list)
+{
+    if (list->list_type == LIST_NORMAL) {
+        return NULL;
+    } else if (list->list_type == LIST_ASCENDING) {
+        return list->end;
+    } else {
+        return list->begin;
+    }
+}
+
+Node* list_get_min(List* list)
+{
+    if (list->list_type == LIST_NORMAL) {
+        return NULL;
+    } else if (list->list_type == LIST_ASCENDING) {
+        return list->begin;
+    } else {
+        return list->end;
+    }
+}
+
+void list_remove(List* list, Node* node)
+{
+    if (list->begin == node) { //第一个或者只有一个
+        list_remove_begin(list);
+    } else if (list->end == node) { //最后一个
+        list_remove_end(list);
+    } else { //第二个到倒数第二个
+        list->index = list->begin->next;
+        while (list->index != list->end) {
+            if (list->index == node) {
+                list->index->prev->next = list->index->next;
+                list->index->next->prev = list->index->prev;
+                --list->list_length;
+                break;
+            } else {
+                list->index = list->index->next;
+            }
+        }
+    }
+}
+
+void list_insert_ascending(List* list, Node* node)
+{
+    if (list->list_length == 0) {
+        list_insert_first(list, node);
+    } else {
+        if (*list->end->data <= *node->data) { //判断最后一个节点或者是只有一个节点
+            list_insert_end(list, node);
+        } else if (*list->first->data > *node->data) { //判断第一个节点
+            list_insert_begin(list, node);
+        } else { //判断第二个到倒数第二个节点
+            list->index = list->begin->next;
+            while (list->index != list->end) { 
+                if (*list->index->data <= *node->data) {
+                    list->index = list->index->next;
+                } else {
+                    break;
+                }
+            }
+
+            node->next = list->index;
+            node->prev = list->index->prev;
+            list->index->prev->next = node;
+            list->index->prev = node;
+            node->list = list;
+            ++list->list_length;
+        }
+    }
+}
+
+void list_insert_descending(List* list, Node* node)
+{
+    if (list->list_length == 0) {
+        list_insert_first(list, node);
+    } else {
+        if (*list->end->data >= *node->data) { //判断最后一个节点或者是只有一个节点
+            list_insert_end(list, node);
+        } else if (*list->begin->data < *node->data) { //判断第一个节点
+            list_insert_begin(list, node);
+        } else { //判断第二个到倒数第二个节点
+            list->index = list->begin->next
+            while (list->index != list->end) {
+                if (*list->index->data >= *node->data) {
+                    list->index = list->index->next;
+                } else {
+                    break;
+                }
+            }
+
+            node->next = list->index;
+            node->prev = list->index->prev;
+            list->index->prev->next = node;
+            list->prev = node;
+            node->list = list;
+            ++list->list_length;
+        }
+    }
+}
---
+++ task/aos_stm8_iar/common_list.h
@@ -0,0 +1,38 @@
+#ifndef _COMMON_LIST_H_
+#define _COMMON_LIST_H_
+
+#include <stdint.h>
+
+typedef struct {
+    void* data;
+    Node* prev;
+    Node* next;
+    List* list;
+} Node;
+
+typedef struct {
+    uint8_t list_type;
+    uint16_t list_length;
+    Node* begin;
+    Node* end;
+    Node* index;
+} List;
+
+enum ListType
+{
+    LIST_NORMAL,
+    LIST_ASCENDING,
+    LIST_DESCENDING
+};
+
+void list_init(List* list, uint8_t list_type);
+void list_insert_end(List* list, Node* node);
+void list_insert_begin(List* list, Node* node);
+Node* list_get_max(List* list);
+Node* list_get_min(List* list);
+void list_remove(List* list, Node* node);
+//下面两次接口在一个程序中只能使用一个，要么升序要么降序，不能同时使用
+void list_insert_ascending(List* list, Node* node);
+void list_insert_descending(List* list, Node* node);
+
+#endif
